1b8ca0c56eb08be45deb9a38fa95445ce6671da2
tmangowal <tmangowal@gmail.com>
2020-02-21 16:53:05 +0700

    [NEW] Added exercises

---
+++ Fundamental/WeekendTasks/WeekendTask1.js
@@ -0,0 +1,37 @@
+
+// 1. Function utk hapus huruf vocal
+// 2. Function hitung uang parkir
+// 3. Function ganjil genap 
+// 4. Function display angka terbesar 
+// 5. Function ngefilter string (hanya bisa integer)
+// 6. Function untuk hapus huruf awal dan akhir saja 
+
+// 1
+function hapusVocal(kata){
+    // write your code here
+}
+
+// 2
+function parkir(jam){
+    // write your code here
+}
+
+// 3
+function ganjilGenap(angka){
+    // write your code here
+}
+
+// 4
+function angkaTerbesar(angka1, angka2, angka3){
+    // write your code here
+}
+
+// 5
+function filterString(input){
+    // write your code here
+}
+
+// 6
+function removeChar(string){
+    // write your code here
+}
