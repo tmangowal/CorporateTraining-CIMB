a0834fcce63266a227ec02f99cc83851cd33883c
tmangowal <tmangowal@gmail.com>
2020-02-25 11:24:45 +0700

    [NEW] Day 2 update

---
+++ Fundamental/Course/Day-1/file.js
@@ -0,0 +1,37 @@
+// var container = '1'
+// var container = 3
+
+// var, let, const
+
+// let container = "Batman's city is called" + '"Gotham"'
+// container = 3
+
+// const gravitasi = 9.8
+// gravitasi = 'halo'
+
+
+// "Gotham" is the city of Batman
+
+// let city = '"Gotham"'
+// let owner = "Batman"
+
+// let result = city + ' is the city of ' + owner 
+
+// console.log(result)
+
+// console.log(city, owner, "halo")
+
+// let namaDepan = "Theo"
+// let namaBelakang = "Tampan"
+
+// let satu = '1'
+// let dua = 2
+
+// console.log(satu - dua)
+
+// console.log(namaDepan + ' ' +namaBelakang)
+
+let response = 'wadidaw'
+let pajak = 5000
+
+console.log(response.length)
---
+++ Fundamental/Course/Day-2/index.js
@@ -0,0 +1,20 @@
+
+
+
+// let benarSalah = true
+
+// // console.log(3 > 5)
+
+// if (3 > 5) {
+//   console.log('')
+// } else {
+//   console.log('Program Selesai')
+// }
+
+// if (0) {
+//   console.log('Masuk If')
+// }
+
+// console.log(15 % 4)
+
+console.log((3 < 5 || 5 < 3) && 7 > 9)
