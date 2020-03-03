edb14b1be67ff7f21345750263f3af26e609f534
tmangowal <tmangowal@gmail.com>
2020-03-03 10:06:10 +0700

    pr day 6

---
+++ Fundamental/Course/Day-7/index.js
@@ -0,0 +1,63 @@
+// function gakReturn () {
+//   let a, b;
+
+//   a + b
+// }
+
+// function adaReturn () {
+//   return 'HELLOOOOO'
+// }
+
+// function tambah(a, b) {
+//    a + b
+// }
+
+// let c;
+
+// function ubahC () {
+//   c = 3
+// }
+
+// ubahC()
+
+// console.log(1 + 2 + c)
+
+// function tambah1() {
+//   return 3
+// }
+
+// function tambah2(a) {
+//   return a
+// }
+
+// console.log(tambah1() + tambah2(2))
+
+// let arr = [1, 2, 3, 4];
+
+// let obj = {
+//   tahun: 2000,
+//   nama: "DOraemon",
+//   kontak: {
+//     rumah: "Jl jalanan",
+//     telfon: [081234, 084321]
+//   }
+// };
+
+// function testing() {
+//   return {
+//     tahun: 2000,
+//     nama: "DOraemon",
+//     kontak: {
+//       rumah: "Jl jalanan",
+//       telfon: [081234, 084321]
+//     }
+//   };
+// }
+
+// console.log(testing().kontak.telfon.forEach());
+
+function tambah(a, b) {
+  return a + b
+}
+
+console.log(tambah(3, 7))
