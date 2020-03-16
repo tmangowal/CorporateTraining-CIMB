f28abde4810af64720fc4919916a054eb79317da
tmangowal <tmangowal@gmail.com>
2020-03-16 15:45:43 +0700

    corona exercise

---
+++ Fundamental/Exercises/corona-week/day-1.js
@@ -0,0 +1,20 @@
+const tripleTrouble = (one, two, three) => {
+  // Buatlah sebuah function yang menerima 3 string
+  // Input: "aa", "bb", "cc"
+  // Output: "abcabc"
+
+  // Input: "ab", "cd", "ef"
+  // Output: "acebdf"
+
+  // write your code here
+}
+
+const altCaps = (string) => {
+  // Buatlah sebuah function yang menerima sebuah string
+  // Outputnya sebuah array yang index pertama adalah semua 
+  // huruf yg posisinya ganjil menjadi kapital, index kedua
+  // semua huruf yg posisinya genap jadi kapital
+
+  // Input: "abcdef"
+  // Output: ["AbCdEf", "aBcDeF"]
+}
